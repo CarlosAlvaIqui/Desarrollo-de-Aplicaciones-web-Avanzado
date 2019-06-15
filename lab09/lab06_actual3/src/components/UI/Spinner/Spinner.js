0b20112a29883d2ee4840d3227644d6b853d0615
CarlosAlvaIqui <carlos.alvarez.i@tecsup.edu.pe>
2019-06-15 18:38:10 -0500

    reenvio de lab de la pagina

---
+++ lab09/lab06_actual3/src/components/UI/Spinner/Spinner.js
@@ -0,0 +1,18 @@
+import React, { Fragment } from 'react';
+import './../Spinner/Spinner.css';
+
+const Spinner = () => {
+    return (
+        <Fragment>
+            <div class="lds-facebook">
+                <div>
+                </div>
+                <div>
+                </div>
+                <div>
+                </div>
+            </div>
+        </Fragment>
+    )
+}
+export default Spinner;
